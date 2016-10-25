9c658adde4d98b389f7e3bbd95f664f1801216f6
Stefan Rishøj Hansen <stef6635@edu.easj.dk>
2016-10-25 14:50:29 +0200

    Tilføjet if-loop som udskriver forskellige ting alt efter BMI'en.

--- Sandbox/BodyAnalyser.cs
+++ Sandbox/BodyAnalyser.cs
@@ -8,7 +8,7 @@ namespace Sandbox
     class BodyAnalyser
     {
         // Given the weight and height of a person, return the BMI
-        public double CalculateBMI(double heightInMeters, double weightInKilo)
+        private double CalculateBMI(double heightInMeters, double weightInKilo)
         {
             double bmi = weightInKilo / (heightInMeters * heightInMeters);
             return bmi;
@@ -16,12 +16,36 @@ namespace Sandbox
 
         // Given the weight and height of a person, return a brief analysis
         // of the BMI of that person
-        public String AnalyseBMI(double heightInMeters, double weightInKilo)
+        public void AnalyseBMI(double heightInMeters, double weightInKilo)
         {
             // The below code must be changed
-            String analysis = "All people are beautiful, YOU are beautiful!";
+            if(CalculateBMI(heightInMeters, weightInKilo) < 15)
+            {
+                Console.WriteLine("You are way too skinny!");
+            }
 
-            return analysis;
+            else if ((CalculateBMI(heightInMeters,weightInKilo) >= 15) && (CalculateBMI(heightInMeters, weightInKilo) < 22))
+            {
+                Console.WriteLine("You are a bit skinny");
+            }
+
+            else if ((CalculateBMI(heightInMeters, weightInKilo) >= 22) && (CalculateBMI(heightInMeters,weightInKilo) < 28))
+            {
+                Console.WriteLine("You are just fine!");
+            }
+
+            else if ((CalculateBMI(heightInMeters, weightInKilo) >= 28) && (CalculateBMI(heightInMeters, weightInKilo) < 35))
+            {
+                Console.WriteLine("You are a bit overweight!");
+            }
+            else if (CalculateBMI(heightInMeters, weightInKilo) >= 35)
+            {
+                Console.WriteLine("You are way too fat");
+            }
+
+            Console.WriteLine("All people are beautiful, YOU are beautiful!");
+
+            ;
         }
 
     }
--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -7,7 +7,8 @@ namespace Sandbox
         public void MyCode()
         {
             // The FIRST line of code should be BELOW this line
-
+            BodyAnalyser bmi = new BodyAnalyser();
+            bmi.AnalyseBMI(1.70, 44.00);
             // The LAST line of code should be ABOVE this line
         }
     }
